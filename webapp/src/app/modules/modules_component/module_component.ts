ef55d3a2748f36081bd537a301737e70c2c26385
Chelsea Lee <chelsea.d.lee12@gmail.com>
2017-07-23 21:34:00 -0700

    add module component and module layout component structure

---
+++ webapp/src/app/modules/modules_component/module_component.ts
@@ -0,0 +1,11 @@
+import * as Vue from "vue";
+import Component from "vue-class-component";
+
+require('./module_component.scss');
+
+@Component({
+    template: require('./module_component.tpl.html')
+})
+export class ModuleComponent extends Vue {
+
+}
