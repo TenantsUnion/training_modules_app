98f5c1f0fbaa3c62a4f2e0f951251d5c67aae85d
Chelsea Lee <chelsea.d.lee12@gmail.com>
2017-07-30 22:09:58 -0700

    add module layout components and sass files

---
+++ webapp/src/app/modules/module_details_component/module_details_component.ts
@@ -0,0 +1,9 @@
+import Vue from 'vue';
+import Component from "vue-class-component";
+
+@Component({
+	template: require('./module_details_component.tpl.html')
+})
+
+export class ModuleDetails extends Vue {
+}
--- webapp/src/app/router.ts
+++ webapp/src/app/router.ts
@@ -9,8 +9,4 @@ let router = new VueRouter({});
 router.addRoutes(loginRoutes);
 router.addRoutes(adminRoutes);
 router.addRoutes(userRoutes);
-
-//todo  when going to '/' check if user is logged in and if so go to user page, if not go to findAccountByUsername
-
-
 export const appRouter = router;
