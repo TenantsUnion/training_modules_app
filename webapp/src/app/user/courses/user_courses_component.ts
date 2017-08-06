93402b6a088fb2fac405bf0b4abe8f34007b2d5b
Eric Garasky <egarasky@gmail.com>
2017-08-06 16:03:07 -0700

    Separate html template file for user courses component

--- webapp/src/app/user/courses/user_courses_component.ts
+++ webapp/src/app/user/courses/user_courses_component.ts
@@ -1,6 +1,6 @@
 import Component from "vue-class-component";
 import Vue from "vue";
-import {appRouter} from "../router";
+import {appRouter} from "../../router";
 
 @Component({
     data: () => {
@@ -12,60 +12,7 @@ import {appRouter} from "../router";
     props: {
         username: String
     },
-    // language=HTML
-    template: `
-        <div id="page-content-wrapper">
-            <div class="main">
-                <div class="grid-x">
-                    <div class="small-12 cell">
-                        <div class="callout">
-                            <h4>Enrolled in courses
-                                <button class="button primary"
-                                        @click="enrollInCourse">
-                                    Enroll in a Course
-                                </button>
-                            </h4>
-                            <ul v-if="enrolledInCourses.length"
-                                class="content-list">
-                                <li v-for="course in enrolledInCourses"
-                                    class="content-item">
-                                    <span>{{ course.name }}</span>
-                                    <button class="button primary"
-                                            @click="go(course.id)">Go
-                                    </button>
-                                </li>
-                            </ul>
-                        </div>
-                    </div>
-                </div>
-                <div class="grid-x">
-                    <div class="small-12 cell">
-                        <div class="small-12 cell">
-                            <div class="callout">
-                                <h4>Admin Courses
-                                    <button class="button primary"
-                                            @click="createCourse">Create a
-                                        Course
-                                    </button>
-                                </h4>
-                                <ul v-if="adminOfCourses.length"
-                                    class="content-list">
-                                    <li v-for="course in enrolledInCourses"
-                                        class="content-item">
-                                        <span>{{ course.name }}</span>
-                                        <button class="button primary"
-                                                @click="goToCourse(course.id)">
-                                            Go
-                                        </button>
-                                    </li>
-                                </ul>
-                            </div>
-                        </div>
-                    </div>
-                </div>
-            </div>
-        </div>
-    `
+    template: require('./users_courses_component.tpl.html')
 })
 export class UserCoursesComponent extends Vue {
     enrolledInCourses: ICourseInfo[];
