a092fead445fa125f6da566fae95f458ce6b57a6
Chelsea Lee <chelsea.d.lee12@gmail.com>
2017-08-05 23:20:37 -0700

    User header now reflects active route

--- webapp/src/app/user/header/user_header_component.ts
+++ webapp/src/app/user/header/user_header_component.ts
@@ -1,6 +1,7 @@
 import Component from "vue-class-component";
 import Vue from "vue";
 import {appRouter} from "../../router";
+import {Route} from "vue-router";
 
 
 require('./_user_header_component.scss');
@@ -16,8 +17,12 @@ require('./_user_header_component.scss');
                 <nav class="grid-x">
                     <div class="menu-section cell small-9 align-middle">
                         <ul class="menu">
-                            <li><router-link :to="{name:'courses'}">Courses</router-link></li>
-                            <li><router-link :to="{name:'content'}">Content</router-link></li>
+                            <li v-bind:class="{'is-active': coursesActive}">
+                                <router-link :to="{name:'courses'}">Courses</router-link>
+                            </li>
+                            <li v-bind:class="{'is-active': contentActive}">
+                                <router-link :to="{name:'content'}">Content</router-link>
+                            </li>
                         </ul>
                     </div>
                     <div class="menu-section auto cell align-middle user-actions">
@@ -32,6 +37,16 @@ require('./_user_header_component.scss');
 
 })
 export class AppHeader extends Vue {
+    $route: Route;
+
+    get coursesActive() {
+        return this.$route.path.indexOf('/courses') != -1;
+    }
+
+    get contentActive() {
+        return this.$route.path.indexOf('/content') != -1;
+    }
+
     signout() {
         //todo hook up to server
         appRouter.push('/');
