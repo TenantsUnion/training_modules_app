4bd097ae79275a4ce6bca4d304b8abd0c52d20bd
Eric Garasky <egarasky@gmail.com>
2017-08-09 23:50:16 -0700

    Loading component that is font awesome spinner

---
+++ webapp/src/app/components/loading.ts
@@ -0,0 +1,13 @@
+import Vue from 'vue';
+import Component from 'vue-class-component';
+
+@Component({
+    template: `
+        <span>
+        <i class="fa fa-spinner fa-spin fa-fw margin-1"></i>
+        <span class="sr-only">Loading...</span>
+</span>
+    `
+})
+export class LoadingComponent extends Vue {
+}
--- webapp/src/app/globals.ts
+++ webapp/src/app/globals.ts
@@ -1,5 +1,7 @@
 import jQuery from 'jquery';
 import Quill from 'quill';
+import Vue from 'vue';
+import {LoadingComponent} from './components/loading';
 
 //put jquery on global window for debugging,
 //workaround for webpack doing this with module number prefixes
@@ -8,5 +10,9 @@ unTypedWindow.jQuery = jQuery;
 unTypedWindow.$ = jQuery;
 
 unTypedWindow.Quill = Quill;
+
+//load global components
+Vue.component('loading', LoadingComponent);
+
 
 export const $ = jQuery;
