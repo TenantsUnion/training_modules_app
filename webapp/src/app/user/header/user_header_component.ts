cb472af00d311cb60e8d83bd3ed1680df99229aa
Chelsea Lee <chelsea.d.lee12@gmail.com>
2017-09-25 21:19:40 -0700

    switch everything to flex grid layout

--- webapp/src/app/user/header/user_header_component.ts
+++ webapp/src/app/user/header/user_header_component.ts
@@ -10,34 +10,32 @@ import {Route} from "vue-router";
     // language=HTML
     template: `
         <div class="user-header-wrapper">
-            <div class="inner">
-                <nav class="grid-x">
-                    <div class="menu-section cell small-9 align-middle">
-                        <ul class="menu">
-                            <li v-bind:class="{'is-active': adminCoursesActive}">
-                                <router-link :to="{name: 'adminCourses'}">
-                                    Admin Courses
-                                </router-link>
-                            </li>
-                            <li v-bind:class="{'is-active': enrolledCoursesActive}">
-                                <router-link :to="{name: 'enrolledCourses'}">
-                                    Enrolled Courses
-                                </router-link>
-                            </li>
-                            <li v-bind:class="{'is-active': contentActive}">
-                                <router-link :to="{name:'content'}">Content
-                                </router-link>
-                            </li>
-                        </ul>
-                    </div>
-                    <div class="menu-section auto cell align-middle user-actions">
-                        <span>Welcome, {{ username }}!</span>
-                        <button class="button secondary" @click="signout">Sign
-                            Out
-                        </button>
-                    </div>
-                </nav>
-            </div>
+            <nav class="row">
+                <div class="menu-section columns">
+                    <ul class="menu">
+                        <li v-bind:class="{'is-active': adminCoursesActive}">
+                            <router-link :to="{name: 'adminCourses'}">
+                                Admin Courses
+                            </router-link>
+                        </li>
+                        <li v-bind:class="{'is-active': enrolledCoursesActive}">
+                            <router-link :to="{name: 'enrolledCourses'}">
+                                Enrolled Courses
+                            </router-link>
+                        </li>
+                        <li v-bind:class="{'is-active': contentActive}">
+                            <router-link :to="{name:'content'}">Content
+                            </router-link>
+                        </li>
+                    </ul>
+                </div>
+                <div class="menu-section columns shrink user-actions">
+                    <span>Welcome, {{ username }}!</span>
+                    <button class="button secondary" @click="signout">Sign
+                        Out
+                    </button>
+                </div>
+            </nav>
         </div>
     `
 
