1d1e8337dd595eed0a08921522aa5e9b1efae8c4
Eric Garasky <egarasky@gmail.com>
2017-10-19 09:37:46 -0700

    Edit section component doesn't initiate validation component until done loading to avoid race condition.

--- webapp/src/app/courses/modules/sections/edit/edit_section_component.ts
+++ webapp/src/app/courses/modules/sections/edit/edit_section_component.ts
@@ -1,91 +1,87 @@
+import Vue from 'vue';
 import Component from 'vue-class-component';
 import {QuillComponent} from '../../../../quill/quill_component';
-import Vue from 'vue';
 import {coursesService} from '../../../courses_service';
 import * as VueForm from '../../../../vue-form';
 import {COURSES_ROUTE_NAMES} from '../../../courses_routes';
 import {ViewSectionQuillData} from '../../../../../../../shared/sections';
 import {CourseRefreshComponent} from '../../../../global_components/refresh_route';
+import * as _ from "underscore";
 
 @Component({
     data: () => {
         return {
-            section: {},
+            section: {
+                title: '',
+                description: '',
+                timeEstimate: ''
+            },
             loading: false,
             errorMessages: null,
             formstate: {}
         };
     },
     extends: CourseRefreshComponent,
+    template: require('./edit_section_component.tpl.html'),
     components: {
         'quill-editor': QuillComponent
-    },
-    template: require('./edit_section_component.tpl.html')
+    }
 })
 export class EditSectionComponent extends Vue {
     loading: boolean;
     errorMessages: { [index: string]: string };
-    quillEditor: QuillComponent;
     formstate: VueForm.FormState;
     section: ViewSectionQuillData;
-    currentSectionLoaded: Promise<ViewSectionQuillData>;
+    sectionUnsubscribe;
 
     created() {
         this.loading = true;
-        this.currentSectionLoaded = coursesService.getCurrentSection();
-    }
-
-    mounted() {
-        this.quillEditor = <QuillComponent> this.$refs.editor;
-        this.currentSectionLoaded.then((section) => {
+        this.sectionUnsubscribe = coursesService.subscribeCurrentSection((section) => {
             this.loading = false;
-            this.section = section;
-            this.quillEditor.setQuillEditorContents(this.section.content[0].editorJson);
+            this.section = _.extend({}, section);
         });
-
-
     }
 
-    saveSection() {
+    destroyed() {
+        this.sectionUnsubscribe();
+    }
+
+    async saveSection() {
         this.formstate._submit();
         if (this.formstate.$invalid) {
             return;
         }
 
-        let quillData: Quill.DeltaStatic = this.quillEditor.getQuillEditorContents();
         this.loading = true;
         this.errorMessages = null;
 
-        (async () => {
-            let course = await coursesService.getCurrentCourse();
-            let module = await coursesService.getCurrentModule();
+        let course = await coursesService.getCurrentCourse();
+        let module = await coursesService.getCurrentModule();
 
-            try {
-                await coursesService.saveSection({
-                    id: this.section.id,
-                    courseId: course.id,
-                    moduleId: module.id,
-                    title: this.section.title,
-                    description: this.section.description,
-                    timeEstimate: this.section.timeEstimate,
-                    content: [{
-                        id: this.section.content[0].id,
-                        editorJson: quillData
-                    }]
+        try {
+            await coursesService.saveSection({
+                id: this.section.id,
+                courseId: course.id,
+                moduleId: module.id,
+                title: this.section.title,
+                description: this.section.description,
+                timeEstimate: this.section.timeEstimate,
+                content: [{
+                    // support more than 1 quill content but just present 1 for now
+                    id: this.section.content[0].id,
+                    editorJson: (<QuillComponent> this.$refs.editor).getQuillEditorContents()
+                }]
+            });
+            this.$router.push({
+                name: COURSES_ROUTE_NAMES.viewSection,
+                params: {sectionTitle: this.section.title}
+            });
 
-                });
-                // to do now what
-                this.$router.push({
-                    name: COURSES_ROUTE_NAMES.viewSection,
-                    params: {sectionTitle: this.section.title}
-                });
-
-            } catch (errorMessages) {
-                this.errorMessages = errorMessages;
-            } finally {
-                this.loading = false;
-            }
-
-        })();
+        } catch (errorMessages) {
+            console.error(errorMessages);
+            this.errorMessages = errorMessages;
+        } finally {
+            this.loading = false;
+        }
     }
 }
