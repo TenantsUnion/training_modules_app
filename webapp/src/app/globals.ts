2653ac37bd10ae081831bbf12c73e5ece8439b47
Eric Garasky <egarasky@gmail.com>
2017-10-19 10:19:11 -0700

    Remove empty string values from post request bodies with axios client interceptor.

--- webapp/src/app/globals.ts
+++ webapp/src/app/globals.ts
@@ -3,6 +3,8 @@ import Quill from 'quill';
 import Vue from 'vue';
 import {LoadingComponent} from './global_components/loading';
 import {TimeEstimateComponent} from './global_components/time_estimate/time_estimate_component';
+import * as _ from "underscore";
+import axios, {AxiosRequestConfig} from "axios";
 
 //put jquery on global window for debugging,
 //workaround for webpack doing this with module number prefixes
@@ -17,5 +19,20 @@ Vue.component('loading', LoadingComponent);
 Vue.component('time-estimate', TimeEstimateComponent);
 
 
-
 export const $ = jQuery;
+
+// have properties that are empty strings be deleted from ajax requests
+// empty model values of '' will be used as invalid values otherwise and default behavior
+// of setting value to null is interpreted as string
+axios.interceptors.request.use((config: AxiosRequestConfig) => {
+    return config.data ?
+        _.extend({}, config, {
+            data: _.reduce(Object.keys(config.data), (acc, key) => {
+                if (!_.isString(config.data[key]) || config.data[key]) {
+                    acc[key] = config.data[key];
+                }
+                return acc;
+            }, {})
+        }) :
+        config;
+});
