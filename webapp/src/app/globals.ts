2cfd4465e5b70b629ac657f7c5981c94bd764f5a
Eric Garasky <egarasky@gmail.com>
2017-10-31 13:44:45 -0700

    Register global quill-component for Vue

--- webapp/src/app/globals.ts
+++ webapp/src/app/globals.ts
@@ -5,6 +5,7 @@ import {LoadingComponent} from './global_components/loading';
 import {TimeEstimateComponent} from './global_components/time_estimate/time_estimate_component';
 import * as _ from "underscore";
 import axios, {AxiosRequestConfig} from "axios";
+import {QuillComponent} from './quill/quill_component';
 
 //put jquery on global window for debugging,
 //workaround for webpack doing this with module number prefixes
@@ -17,13 +18,14 @@ unTypedWindow.Quill = Quill;
 //load global components
 Vue.component('loading', LoadingComponent);
 Vue.component('time-estimate', TimeEstimateComponent);
+Vue.component('quill-editor', QuillComponent);
 
 
 export const $ = jQuery;
 
 // have properties that are empty strings be deleted from ajax requests
-// empty model values of '' will be used as invalid values otherwise and default behavior
-// of setting value to null is interpreted as string
+// empty model values of '' will be sent as invalid values otherwise unless checked for in each component
+// and assigning null is saved as text
 axios.interceptors.request.use((config: AxiosRequestConfig) => {
     return config.data ?
         _.extend({}, config, {
