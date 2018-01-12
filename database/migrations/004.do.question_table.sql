bbbe3e9bf1dad92d682d0610b0cc87c87baa5c05
Eric Garasky <egarasky@gmail.com>
2018-01-12 13:41:49 -0800

    Update question and option sql table: -questions track options -no longer have required boolean -keep track of created at and last modified

--- database/migrations/004.do.question_table.sql
+++ database/migrations/004.do.question_table.sql
@@ -1,12 +1,19 @@
 CREATE TABLE tu.question (
-  id                BIGINT PRIMARY KEY,
-  version           BIGINT       NOT NULL DEFAULT 0,
-  question_quill_id BIGINT REFERENCES tu.quill_data (id) UNIQUE,
-  question_type     VARCHAR(100) NOT NULL,
-  answer_type       VARCHAR(100) NOT NULL,
-  required          BOOLEAN               DEFAULT TRUE,
-  created_at        TIMESTAMP    NOT NULL DEFAULT now(),
-  last_modified_at  TIMESTAMP    NOT NULL DEFAULT now()
+  id                     BIGINT PRIMARY KEY,
+  version                BIGINT       NOT NULL DEFAULT 0,
+  question_quill_id      BIGINT REFERENCES tu.quill_data (id) UNIQUE,
+  question_type          VARCHAR(100) NOT NULL,
+  answer_type            VARCHAR(100) NOT NULL,
+  --references id pk column of tu.question_option
+  --correct_option_ids should be subset of option_ids
+  correct_option_ids     BIGINT []    NOT NULL DEFAULT ARRAY [] :: BIGINT [],
+  --references id pk column of tu.question_option
+  option_ids             BIGINT []    NOT NULL DEFAULT ARRAY [] :: BIGINT [],
+  randomize_option_order BOOLEAN      NOT NULL DEFAULT TRUE,
+  answer_in_order        BOOLEAN      NOT NULL DEFAULT FALSE,
+  can_pick_multiple      BOOLEAN      NOT NULL DEFAULT FALSE,
+  created_at             TIMESTAMP    NOT NULL DEFAULT now(),
+  last_modified_at       TIMESTAMP    NOT NULL DEFAULT now()
 );
 
 CREATE INDEX question_question_type_idx
--- database/migrations/005.do.question_option_table.sql
+++ database/migrations/005.do.question_option_table.sql
@@ -1,13 +1,14 @@
 CREATE TABLE tu.question_option (
   id                   BIGINT PRIMARY KEY,
   version              BIGINT                                  NOT NULL DEFAULT 0,
-  question_id          BIGINT REFERENCES tu.question (id)      NOT NULL,
-  answer               BOOLEAN                                          DEFAULT FALSE,
   option_quill_id      BIGINT REFERENCES tu.quill_data (id)    NOT NULL,
   explanation_quill_id BIGINT REFERENCES tu.quill_data (id)    NOT NULL,
   created_at           TIMESTAMP                               NOT NULL DEFAULT now(),
   last_modified_at     TIMESTAMP                               NOT NULL DEFAULT now()
 );
 
-CREATE INDEX question_option_question_id_idx
-  ON tu.question_option (question_id);
+CREATE INDEX question_option_quill_id_idx
+  ON tu.question_option (option_quill_id);
+
+CREATE INDEX question_explanation_quill_id_idx
+  ON tu.question_option (explanation_quill_id);
