8b4ae77caefd50d165fba6911250b1624bb1ed69
Eric Garasky <egarasky@gmail.com>
2018-01-26 10:05:56 -0800

    Make mocha test hook not depend on working directory of running process.

--- test/test_hook.js
+++ test/test_hook.js
@@ -1,7 +1,8 @@
+const path = require('path');
 const moduleAlias = require("module-alias");
 // Override module path alias set in package.json for running server
 // since the directory structure for tests doesn't match.
 moduleAlias.addAlias('@shared', __dirname + '/../shared');
 require("ts-node").register({
-    project: "test/tsconfig.json"
+    project: path.resolve(__dirname, "./tsconfig.json")
 });
