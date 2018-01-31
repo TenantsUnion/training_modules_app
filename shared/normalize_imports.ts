935b250946cc11cfd7965b6a2c83b6089d0a42cf
Eric Garasky <egarasky@gmail.com>
2018-01-31 06:32:46 -0800

    Import moment library with 'require' since es6 syntax doesn't work with default function exports.

--- shared/normalize_imports.ts
+++ shared/normalize_imports.ts
@@ -7,3 +7,17 @@ import DeltaStatic = Quill.DeltaStatic;
 
 export const config = esConfig ? esConfig : require('config');
 export const Delta: DeltaStatic = esDelta ? esDelta : require('quill-delta');
+
+/**
+ * Import moment with node's require instead of es6 import syntax since the top level default export is a callable
+ * function. From the typescript docs
+ * {@link https://www.typescriptlang.org/docs/handbook/declaration-files/templates/module-function-d-ts.html}
+ *
+ *~ Note that ES6 modules cannot directly export callable functions.
+ *~ This file should be imported using the CommonJS-style:
+ *~   import x = require('someLibrary');
+ *~
+ *~ Refer to the documentation to understand common
+ *~ workarounds for this limitation of ES6 modules.
+ */
+export const moment = require('moment');
