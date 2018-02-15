6e781e05c512cee7e2e113726b7d49b3f5c94aef
Eric Garasky <egarasky@gmail.com>
2018-02-15 14:23:42 -0800

    Update remove in place compiled js files script with new test dirs

--- bin/rm_ide_compiled_ts.sh
+++ bin/rm_ide_compiled_ts.sh
@@ -2,5 +2,6 @@
 
 find './server/src' -name "*.js" -exec rm -rf "{}" \; -o -name "*.js.map" -exec rm -rf "{}" \;
 find './webapp/src' -name "*.js" -exec rm -rf "{}" \; -o -name "*.js.map" -exec rm -rf "{}" \;
-find './test/src' -name "*.js" -exec rm -rf "{}" \; -o -name "*.js.map" -exec rm -rf "{}" \;
+find './mocha_unit_test/src' -name "*.js" -exec rm -rf "{}" \; -o -name "*.js.map" -exec rm -rf "{}" \;
+find './testcafe_e2e_test/src' -name "*.js" -exec rm -rf "{}" \; -o -name "*.js.map" -exec rm -rf "{}" \;
 find './shared' -name "*.js" -exec rm -rf "{}" \; -o -name "*.js.map" -exec rm -rf "{}" \;
