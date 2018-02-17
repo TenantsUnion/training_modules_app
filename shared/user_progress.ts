bcf950cbd4c235a9ca42b827bc71aff8c7eae559
Eric Garasky <egarasky@gmail.com>
2018-02-17 15:02:56 -0800

    User progress types.

---
+++ shared/user_progress.ts
@@ -0,0 +1,41 @@
+
+export interface TrainingProgress {
+    id: string;
+    version: number;
+    viewedContentIds: string[],
+    contentIds: string[]
+    correctQuestionIds: string[],
+    submittedQuestionIds: string[]
+    lastViewedAt: string | null;
+    lastModifiedAt: string;
+    createdAt: string;
+}
+
+export interface ContentProgress {
+    quillId: string;
+    viewed: boolean;
+}
+
+export interface QuestionProgress {
+    questionId: string;
+    answered: QuestionAnswered;
+}
+
+enum QuestionAnswered {
+    NOT_ATTEMPTED = 'NOT_ATTEMPTED',
+    ATTEMPTED = 'SUBMITTED',
+    CORRECT = 'CORRECT'
+}
+
+export interface CourseProgress extends TrainingProgress {
+    userId: string;
+    modules: ModuleProgress
+}
+
+export type CourseProgressId = { courseId: string, userId: string };
+
+export interface ModuleProgress extends TrainingProgress {
+    sections: SectionProgress;
+}
+
+export interface SectionProgress extends TrainingProgress {}
