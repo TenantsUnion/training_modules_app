c9c0fcc27abe9e21ae93ccd9653052f00c3885c0
Eric Garasky <egarasky@gmail.com>
2018-03-26 19:10:43 -0700

    Update dependencies. Move common properties to base tsconfig update ignore and scripts for project structure changes.

--- bin/rm_ide_compiled_ts.sh
+++ bin/rm_ide_compiled_ts.sh
@@ -2,6 +2,8 @@
 
 find './server/src' -name "*.js" -exec rm -rf "{}" \; -o -name "*.js.map" -exec rm -rf "{}" \;
 find './webapp/src' -name "*.js" -exec rm -rf "{}" \; -o -name "*.js.map" -exec rm -rf "{}" \;
+find './webapp/karma_unit_test/src' -name "*.js" -exec rm -rf "{}" \; -o -name "*.js.map" -exec rm -rf "{}" \;
 find './mocha_unit_test/src' -name "*.js" -exec rm -rf "{}" \; -o -name "*.js.map" -exec rm -rf "{}" \;
 find './testcafe_e2e_test/src' -name "*.js" -exec rm -rf "{}" \; -o -name "*.js.map" -exec rm -rf "{}" \;
+find './test_shared/src' -name "*.js" -exec rm -rf "{}" \; -o -name "*.js.map" -exec rm -rf "{}" \;
 find './shared' -name "*.js" -exec rm -rf "{}" \; -o -name "*.js.map" -exec rm -rf "{}" \;
